2beff9cece2c2cb64e56c5aa7a3eae3d9f7302ce
Vikhyat Gupta <vkhtgupta@gmail.com>
2024-08-27 23:58:11 +0530

    added users module

---
+++ src/users/admin.py
@@ -0,0 +1,21 @@
+"""
+This file contains all the admin configurations for users module
+"""
+
+from django.contrib import admin
+from django.contrib.admin import ModelAdmin
+
+from users.models import User, UserRole
+
+
+@admin.register(User)
+class UserAdmin(ModelAdmin):
+    list_display = ("id", "email", "first_name", "last_name")
+    search_fields = ("email", "first_name", "last_name")
+
+
+@admin.register(UserRole)
+class UserRoleAdmin(ModelAdmin):
+    list_display = ("id", "user", "role")
+    search_fields = ("user__email",)
+    list_filter = ("role",)
---
+++ src/users/models.py
@@ -0,0 +1,74 @@
+"""
+This file contains all the models for users module.
+"""
+
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+from utils.models import BaseModel
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, first_name, password, **kwargs):
+        email = self.normalize_email(email=email)
+        user = self.model(email=email, first_name=first_name, **kwargs)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, first_name, password, **kwargs):
+        kwargs.setdefault("is_staff", True)
+        kwargs.setdefault("is_superuser", True)
+        return self.create_user(
+            email=email, first_name=first_name, password=password, **kwargs
+        )
+
+
+class User(AbstractUser):
+    """
+    This model is used to store all the details for a user.
+    """
+
+    username = None
+    email = models.EmailField(max_length=256, unique=True)
+    first_name = models.CharField(max_length=256)
+    last_name = models.CharField(max_length=256, null=True, blank=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["first_name"]
+
+    objects = CustomUserManager()
+
+    class Meta:
+        verbose_name = "User"
+        verbose_name_plural = "Users"
+
+    def __str__(self):
+        return self.email
+
+    @property
+    def full_name(self):
+        return f"{self.first_name.strip()} {(self.last_name or '').strip()}".rstrip()
+
+
+class UserRole(BaseModel):
+    """
+    This model is used to store all the roles associated with a user.
+    """
+
+    class Role(models.IntegerChoices):
+        STAFF = 1
+        MANAGER = 2
+
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    role = models.PositiveSmallIntegerField(choices=Role.choices)
+
+    class Meta:
+        verbose_name = "User Role"
+        verbose_name_plural = "User Roles"
+        constraints = [
+            models.UniqueConstraint(
+                fields=("user", "role"), name="user_role_unique_constraint"
+            )
+        ]
