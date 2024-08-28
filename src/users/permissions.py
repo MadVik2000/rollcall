6c1c1c6df90432e27c2c7bde9e6e709c2e5918d4
Vikhyat Gupta <vkhtgupta@gmail.com>
2024-08-28 22:03:27 +0530

    added custom permission classes for users module

---
+++ src/users/permissions.py
@@ -0,0 +1,29 @@
+"""
+This file contains all the custom permission classes related to users module
+"""
+
+from rest_framework.permissions import IsAuthenticated
+
+from users.models import UserRole
+
+
+class IsManager(IsAuthenticated):
+    def has_permission(self, request, view):
+        return (
+            super().has_permission(request, view)
+            and request.user
+            and request.user.userrole_set.filter(
+                role=UserRole.Role.MANAGER, date_deleted__isnull=False
+            ).exists()
+        )
+
+
+class IsStaffMember(IsAuthenticated):
+    def has_permission(self, request, view):
+        return (
+            super().has_permission(request, view)
+            and request.user
+            and request.user.userrole_set.filter(
+                role=UserRole.Role.STAFF_MEMBER, date_deleted__isnull=False
+            ).exists()
+        )
