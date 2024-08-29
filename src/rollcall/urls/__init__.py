b1fae333d47d767b013dea0f58a9434e742ce348
Vikhyat Gupta <vkhtgupta@gmail.com>
2024-08-29 17:40:12 +0530

    added static and media urls

--- src/rollcall/urls/__init__.py
+++ src/rollcall/urls/__init__.py
@@ -17,7 +17,12 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from .v1 import urlpatterns as v1_urlpatterns
 
 urlpatterns = [path("admin/", admin.site.urls), path("v1/", include(v1_urlpatterns))]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
