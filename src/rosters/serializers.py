891cf73e32ab259c90579d232cb30297c8f58945
Vikhyat Gupta <vkhtgupta@gmail.com>
2024-08-29 22:57:14 +0530

    added schedule swap request serializer

--- src/rosters/serializers.py
+++ src/rosters/serializers.py
@@ -4,7 +4,7 @@ This file contains all the model serializers for rosters module.
 
 from rest_framework import serializers
 
-from rosters.models import Roster, RosterUserSchedule
+from rosters.models import Roster, RosterUserSchedule, ScheduleSwapRequest
 from users.serializers import UserSerializer
 
 
@@ -20,3 +20,16 @@ class RosterUserScheduleSerializer(serializers.ModelSerializer):
     class Meta:
         model = RosterUserSchedule
         exclude = RosterUserSchedule.LOG_FIELDS
+
+
+class ScheduleSwapRequestSerializer(serializers.ModelSerializer):
+    sender = UserSerializer()
+    receiver = UserSerializer()
+    status = serializers.SerializerMethodField()
+
+    def get_status(self, instance):
+        return ScheduleSwapRequest.Status(instance.status).label
+
+    class Meta:
+        model = ScheduleSwapRequest
+        exclude = ScheduleSwapRequest.LOG_FIELDS
